d96942c63ed2d9fd6ee8b3a0046d2379dba55fb5
kimdahee7 <kikidahee7@duksung.ac.kr>
2024-02-19 17:44:35 +0900

    Feb_3rdWeek_kimdahee7

---
+++ kimdahee7/baekjoon/Silver/11501-주식.py
@@ -0,0 +1,14 @@
+# 정답 코드 참고
+T = int(input())
+for _ in range(T):
+  N = int(input())
+  l = list(map(int,input().split()))
+  l = l[::-1]
+  max = l[0]
+  total = 0
+
+  for i in range(1,N):
+    if max < l[i]:
+      max = l[i]
+    total += max - l[i]
+  print(total)
