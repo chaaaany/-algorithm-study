5c76894703e937eac2e2f50556af30f9b43fcee7
kimdahee7 <kikidahee7@duksung.ac.kr>
2024-03-13 18:45:53 +0900

    Mar_2ndWeek_kimdahee7

---
+++ kimdahee7/programmers/Lv1/덧칠하기.py
@@ -0,0 +1,11 @@
+# 힌트 참고
+def solution(n, m, section):
+    answer = 1
+    start = section[0]
+    total = start + m -1
+    for i in range(1,len(section)):
+        if section[i] > total:
+            start = section[i]
+            total = start + m -1
+            answer += 1
+    return answer
